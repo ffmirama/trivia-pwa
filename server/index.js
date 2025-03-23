d02e6202c3b9e5b0f5309388f967ae05e4c36325
Francisco Mira <ffmirama@gmail.com>
2025-03-23 16:50:40 -0300

    feat: conexión Sequelize con PostgreSQL y sincronización inicial

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
-
+require('./sync');
 const app = express();
 app.use(cors());
 app.use(express.json());
--- server/models/User.js
+++ server/models/User.js
@@ -1,1 +1,28 @@
-// User model
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password_hash: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
